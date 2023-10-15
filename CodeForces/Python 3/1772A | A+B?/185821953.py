eca486a870bdb1ff8cf5f5471743ce65afea11a8
Brian C Ahaneku <72224090+brianahaneku@users.noreply.github.com>
2023-10-15 08:57:19 -0400

    Committing files

---
+++ CodeForces/Python 3/1772A | A+B?/185821953.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 12:08:20 2022
+ 
+@author: brianahaneku
+"""
+import heapq
+import math
+ 
+ 
+def solve():
+    s=input()
+    print(eval(s))
+    return
+ 
+t=int(input())
+for _ in range(t):
+    solve()
