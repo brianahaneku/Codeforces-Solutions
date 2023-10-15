f0e50b05d415aaee715d2ccf57ae1a2b040bcd68
Brian C Ahaneku <72224090+brianahaneku@users.noreply.github.com>
2023-10-15 08:56:54 -0400

    Committing files

---
+++ CodeForces/Python 3/236A | Boy or Girl/187868838.py
@@ -0,0 +1,18 @@
+def solve():
+    
+    s=set([x for x in input()])
+    if len(s)%2==0:
+        print("CHAT WITH HER!")
+    else:
+        print("IGNORE HIM!")
+    
+    
+        
+            
+        
+    return
+            
+    
+t=1
+for _ in range(t):
+    solve()
