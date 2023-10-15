6ab8abc8bb70fb0e7dd4893d013a1371668de2a3
Brian C Ahaneku <72224090+brianahaneku@users.noreply.github.com>
2023-10-15 08:56:50 -0400

    Committing files

---
+++ CodeForces/Python 3/1768A | Greatest Convex/188050617.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 12:08:20 2022
+ 
+@author: brianahaneku
+"""
+import heapq
+import math
+ 
+eps = 10**(-9)
+ 
+# Python3 code for range maximum query and updates
+from math import ceil, log
+ 
+def solve():
+    k=int(input())
+    
+    print(k-1)
+    
+    
+        
+    return
+            
+    
+t=int(input())
+for _ in range(t):
+    solve()
